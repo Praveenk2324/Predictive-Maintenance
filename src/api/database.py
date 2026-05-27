5f516456a69dc4aded2f10559584f25084d9eafa
Praveenk2324 <praveennithishan2005@gmail.com>
2026-05-27 18:37:28 +0530

    Typo Correction

--- src/api/database.py
+++ src/api/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://admin:admin_password@postgres:5432/maintenance_logs"
+SQL_ALCHEMY_DATABASE_URL = "postgresql://admin:admin_password@postgres:5432/maintenance_logs"
 
 engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
